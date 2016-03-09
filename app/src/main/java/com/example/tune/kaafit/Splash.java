6bb5caec0b21c8338dc4aa4ee4408a56ab42c9db
skafis <franciskipchumba5@gmail.com>
2016-03-09 17:46:30 +0300

    add splash screen

---
+++ app/src/main/java/com/example/tune/kaafit/Splash.java
@@ -0,0 +1,31 @@
+package com.example.tune.kaafit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by frank on 3/9/16.
+ */
+public class Splash extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent startmain = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(startmain);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        myThread.start();
+    }
+}
